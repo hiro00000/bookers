ca830f7633040dbb5b4d05335da5b5098b26e185
EC2 Default User <ec2-user@ip-172-31-1-214.ec2.internal>
2022-05-09 12:32:55 +0000

    second-commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,48 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book =Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end  
+  end
+
+  def show
+    @books = Book.find(params[:id])
+  end
+
+  def edit
+    @books = Book.find(params[:id])
+  end
+  
+  def update
+    @books = Book.find(params[:id])
+    @books.save
+    if  @books.update(book_params)
+      redirect_to book_path(@books.id)
+      flash[:success] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    list = Book.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
